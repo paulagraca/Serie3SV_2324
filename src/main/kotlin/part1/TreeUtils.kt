96c0bcdcf368cccf45b62d800a85d9564c5c227c
paulagraca <mpaulagraca@gmail.com>
2024-05-13 12:06:22 +0100

    Initial commit

---
+++ src/main/kotlin/part1/TreeUtils.kt
@@ -0,0 +1,25 @@
+package part1
+
+data class Node<E>(var item: E, var left:Node<E>?, var right:Node<E>?)
+
+fun printIf( root:Node<Int>?, predicate: (Int) -> Boolean ):Int{
+  TODO()
+}
+
+fun countBetween(root: Node<Int>?, min: Int, max: Int): Int {
+    TODO()
+}
+
+fun isChildrenSum(root:Node<Int>?):Boolean{
+   TODO()
+}
+
+
+
+
+
+
+
+
+
+
